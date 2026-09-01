32ca288fcd3eff86d49eec89bb528bdc39164c91
karoliny-a <ana.sobierai.silva@escola.pr.gov.br>
2026-09-01 15:06:42 -0300

    mudei o script

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-/ Seleciona todos os botões da página
-  const botoes = document.querySelectorAll("button");
+const botoes = document.querySelectorAll('button');
 
-  botoes.forEach(function(botao) {
-    botao.addEventListener("click", function() {
-        console.log("fui clicado");
-        let texto = botao.querySelector("span");
-        
-        // Converte o texto atual para número, soma +1 e atualiza o elemento
-        let valorAtual = parseInt(texto.textContent);
-        texto.textContent = valorAtual + 1;
+botoes.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const contador = botao.querySelector('span');
+        let valorAtual = parseInt(contador.textContent);
+        contador.textContent = valorAtual + 1;
     });
-  });
+});
