90431dff15e431b2a80060ee935bcbe7676b51d8
karoliny-a <ana.sobierai.silva@escola.pr.gov.br>
2026-09-02 09:29:12 -0300

    ajuste no js

--- script.js
+++ script.js
@@ -10,18 +10,20 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
+
+    const btnTemaEscuro = document.querySelector('.btn-tema-escuro');
+
+    btnTemaEscuro.addEventListener("click", mudaTema);
+
+    function mudaTema() {
+        const corpoPagina = document.body;
+        if (corpoPagina.classList.contains("tema-escuro")) {
+            corpoPagina.classList.remove("tema-escuro");
+        } else {
+            corpoPagina.classList.add("tema-escuro");
+        }
+    }
 });
 
-const btnTemaEscuro = document.querySelectorAll('.btn-tema-escuro');
 
-btnTemaEscuro.addEventListener("click", mudaTema);
-
-function mudaTema() {
-    const corpoPagina = document.body;
-    if (corpoPagina.classList.contains("tema-escuro")) {
-        corpoPagina.classList.remove("tema-escuro");
-    } else {
-        corpoPagina.classList.add("tema-escuro");
-    }
-}
 
